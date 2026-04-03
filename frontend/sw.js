ed65731c6c8d6dc50061b730bfeee5fc2afb1e3f
Stefan Z <100451534+Zauni1984@users.noreply.github.com>
2026-04-03 17:02:59 +0200

    fix: empty SW completely - remove skipWaiting that caused page reload

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,6 +1,1 @@
-// Service Worker disabled - no caching, no reload
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+// Service Worker intentionally empty
