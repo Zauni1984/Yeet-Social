4c4a0e948570406c5690e0f7fc4f1cca367c2223
Stefan Z <100451534+Zauni1984@users.noreply.github.com>
2026-04-10 22:34:46 +0200

    feat: FeedPost includes tip_total_yeet field

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -60,6 +60,8 @@ pub struct FeedPost {
     pub expires_at: DateTime<Utc>,
     pub created_at: DateTime<Utc>,
     pub author: FeedPostAuthor,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tip_total_yeet: Option<f64>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
